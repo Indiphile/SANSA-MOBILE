c8e14d0dcc824e70d3a223c2da849f7ae87992b3
Indiphile Ngqambuza <ingqambuza@sansa.org.za>
2023-02-21 15:35:36 +0200

    Start

---
+++ app/src/main/java/com/example/login/WelcomeDashboardActivity.java
@@ -0,0 +1,25 @@
+package com.example.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class WelcomeDashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_dashboard);
+        Button wl_btn = findViewById(R.id.welcome_login_btn);
+
+        wl_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(WelcomeDashboardActivity.this,LoginActivity.class));
+            }
+        });
+    }
+}
